ba0b9ec1224e7d4254bc0136b05ae856e587ec94
J Hoffman <theskidster@gmail.com>
2018-04-06 20:43:50 -0500

    added external dependencies and a todo list in Main

--- rng_cpp03_testbed/rng_cpp03_testbed/Main.cpp
+++ rng_cpp03_testbed/rng_cpp03_testbed/Main.cpp
@@ -1,4 +1,19 @@
+#include <glad\glad.h>
+#include <GLFW\glfw3.h>
+#include <iostream>
+
+/**
+ * J Hoffman
+ * 04/06/18
+ * 
+ * todo: 
+ *	- template files
+ *	- auto bullet multiline comments
+ *	- versioning system?
+ */
 
 int main() {
+	
+	
 
 }
