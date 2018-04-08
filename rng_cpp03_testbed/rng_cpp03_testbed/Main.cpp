774cc2621c262b8676ec4a8d8ec7ac73a5d0c54c
J Hoffman <theskidster@gmail.com>
2018-04-08 14:44:31 -0500

    established window, set context, and implemented a simple timestep

--- rng_cpp03_testbed/rng_cpp03_testbed/Main.cpp
+++ rng_cpp03_testbed/rng_cpp03_testbed/Main.cpp
@@ -5,15 +5,33 @@
 /**
  * J Hoffman
  * 04/06/18
- * 
- * todo: 
- *	- template files
- *	- auto bullet multiline comments
- *	- versioning system?
  */
 
+const int WIDTH = 320;
+const int HEIGHT = 224;
+int scale = 2;
+GLFWwindow* wi;
+
 int main() {
-	
-	
+
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); //omits legacy features
+
+	wi = glfwCreateWindow(WIDTH * scale, HEIGHT * scale, "RNG03: testbed", NULL, NULL);
+
+	glfwMakeContextCurrent(wi);
+
+	gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+	glViewport(0, 0, WIDTH, HEIGHT);
+
+	while(!glfwWindowShouldClose(wi)) {
+		glfwSwapBuffers(wi);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 }
